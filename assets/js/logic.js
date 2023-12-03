6a05338c4600bb38a12a5264a8c286c74a1edcd9
Tom Fahy <thomas.fahy9@gmail.com>
2023-12-03 21:21:13 +0000

    add feedback and scoring system to render question function

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -6,7 +6,7 @@ const timeEl = document.querySelector("#time");
 const startButton = document.querySelector("#start");
 const endScreen = document.querySelector("#end-screen");
 
-let lastAnswerCorrect = false;
+let lastAnswerCorrect = null;
 let currentScore = 75;
 
 // timer function
@@ -29,6 +29,20 @@ function setTime() {
   }, 1000);
 }
 
+//function to display feedback on last question. takes div to display feedback in as argument.
+
+function renderFeedback (div) {
+  let lastAnswer = document.createElement("h3");
+  lastAnswer.setAttribute("class", "feedback");
+  if (lastAnswerCorrect && lastAnswerCorrect !== null) {
+    lastAnswer.innerHTML = "Correct!"
+    div.appendChild(lastAnswer);
+  } else if (!lastAnswerCorrect && lastAnswerCorrect !== null) {
+    lastAnswer.innerHTML = "Wrong!"
+    div.appendChild(lastAnswer);
+  }
+}
+
 //render final screen function
 function renderEndScreen () {
   questionDiv.className = "hide";
@@ -38,10 +52,12 @@ function renderEndScreen () {
   printScore.innerHTML = '' + currentScore;
 };
 
+
 //render question function, uses recursive call to keep questions coming
 
 function renderQuestions(myQuestions) {
 
+  console.log(lastAnswerCorrect);
   let currentQuestion = myQuestions.shift(); //remove question from question array, or recursion will not stop
 
   questionDiv.className = "start";
@@ -50,13 +66,21 @@ function renderQuestions(myQuestions) {
   for (let i = 0; i < currentQuestion.answers.length; i++) {
     let questionButton = document.createElement("button");
     questionButton.setAttribute("class", "answerButton");
+    i == currentQuestion.correct_index ? questionButton.setAttribute("id", "correct"): questionButton.setAttribute("id", "wrong"); 
     questionButton.textContent = '' + (i+1) + ". " + currentQuestion.answers[i]; //add number to start of question text
     questionChoices.appendChild(questionButton);
   }
 
+  //display feedback for last answer, unless it is the first question
+  renderFeedback();
+
   const answerButtons = questionChoices.querySelectorAll(".answerButton");
   for (i = 0; i < answerButtons.length; i++) {
     answerButtons[i].addEventListener("click", function() {
+      if (this.id == "correct") {lastAnswerCorrect = true} else { //if correct, change bool and render it as correct next time, otherwise take it off score
+        currentScore -= 15;
+        lastAnswerCorrect = false;
+      }   
       questionChoices.innerHTML = '';
       myQuestions.length > 0 ? renderQuestions(myQuestions) : renderEndScreen(); //if there are still questions, render again, otherwise move to score entry screen.
     });
